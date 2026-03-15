836d8fb4047b562ea7ba61160b3d41711b8f41e2
a.firsova <a.firsova@propellerads.net>
2026-03-15 12:46:01 +0300

    fix

--- tests/success_test.py
+++ tests/success_test.py
@@ -3,20 +3,20 @@ from selene import have, be
 
 
 def test_example_title_success():
-    browser.open('https://example.com')
+    browser.open('/')
     assert browser.driver.title == 'Example Domain'
 
 
 def test_example_h1_success():
-    browser.open('https://example.com')
+    browser.open('/')
     browser.element('h1').should(have.text('Example Domain'))
 
 
 def test_example_element_visible():
-    browser.open('https://example.com')
+    browser.open('/')
     browser.element('p').should(be.visible)
 
 
 def test_example_url_check():
-    browser.open('https://example.com')
+    browser.open('/')
     assert browser.driver.current_url == 'https://example.com/'
