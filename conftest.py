72c800e2e8f9f762e83804d66e16495cb9b65b68
a.firsova <a.firsova@propellerads.net>
2026-03-15 12:38:30 +0300

    fix

--- conftest.py
+++ conftest.py
@@ -1,11 +1,24 @@
 import pytest
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(autouse=True)
 def browser_settings():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    browser.config.driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
+
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    browser.config.headless = False  # True для headless
+
     yield
     browser.quit()
