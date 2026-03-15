aea15976413c69e07bd2b6d9a947a3a43d9ef91e
a.firsova <a.firsova@propellerads.net>
2026-03-15 12:03:11 +0300

    added tests + requirement file

---
+++ tests/fail_tests.py
@@ -0,0 +1,22 @@
+from selene.support.shared import browser
+from selene import have, be
+
+
+def test_example_title_success():
+    browser.open('https://example.com')
+    assert browser.driver.title == 'Example Dommain'
+
+
+def test_example_h1_success():
+    browser.open('https://example.com')
+    browser.element('h1').should(have.text('Example Domamin'))
+
+
+def test_example_element_visible():
+    browser.open('https://exampple.com')
+    browser.element('p').should(be.visible)
+
+
+def test_example_url_check():
+    browser.open('https://example.com')
+    assert browser.driver.current_url == 'https://exampple.com/'
---
+++ tests/success_tests.py
@@ -0,0 +1,22 @@
+from selene.support.shared import browser
+from selene import have, be
+
+
+def test_example_title_success():
+    browser.open('https://example.com')
+    assert browser.driver.title == 'Example Domain'
+
+
+def test_example_h1_success():
+    browser.open('https://example.com')
+    browser.element('h1').should(have.text('Example Domain'))
+
+
+def test_example_element_visible():
+    browser.open('https://example.com')
+    browser.element('p').should(be.visible)
+
+
+def test_example_url_check():
+    browser.open('https://example.com')
+    assert browser.driver.current_url == 'https://example.com/'
